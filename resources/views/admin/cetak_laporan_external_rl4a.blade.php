2d0848e5509055448fc3b744926e00c5024e0582
greenzonessh <greenzonessh@gmail.com>
2017-10-31 13:24:04 +0700

    perbaikan html cetak laporan rl 4a

--- resources/views/admin/cetak_laporan_external_rl4a.blade.php
+++ resources/views/admin/cetak_laporan_external_rl4a.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>SIRI | Invoice</title>
+  <title>SIRI | Laporan Ext RL 4a</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
